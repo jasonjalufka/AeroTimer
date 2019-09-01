37570bf2cf396ed923cdc1714d50e59c0d98ea7f
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-31 17:49:13 -0700

    Add Home Screen, Recipe Page, Timer Page (#3)

---
+++ src/pages/Home.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import recipeData from '../recipes';
+import styled from 'styled-components';
+import { Section, theme } from '../styles';
+import { IconLogo } from '../components/icons';
+
+const HeroSection = styled(Section)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-height: 40vh;
+`;
+
+const Title = styled.h1`
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+
+const RecipeSection = styled(Section)``;
+
+const RecipeTitle = styled.h2`
+  padding-left: 20px;
+  margin-bottom: 0;
+`;
+
+const RecipeList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const RecipeListItem = styled.li`
+  margin: 5px 20px;
+  padding: 20px 30px;
+  border-radius: ${theme.borderRadius};
+  font-size: 28px;
+  font-weight: 300;
+  text-transform: capitalize;
+  box-shadow: 0 2px 10px 0 rgba(167, 167, 167, 0.5);
+  background-color: ${theme.colors.blue};
+  color: ${theme.colors.white};
+`;
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection>
+        <Title>AeroTimer</Title>
+        <IconLogo />
+      </HeroSection>
+      <RecipeSection>
+        <RecipeTitle>Recipes</RecipeTitle>
+        <RecipeList>
+          {Object.keys(recipeData).map((recipe, i) => {
+            return (
+              <RecipeListItem key={i} method={recipeData[recipe].method}>
+                <Link
+                  to={{
+                    pathname: '/recipe/',
+                    state: {
+                      data: recipeData[recipe]
+                    }
+                  }}
+                >
+                  {recipeData[recipe].name}
+                </Link>
+              </RecipeListItem>
+            );
+          })}
+        </RecipeList>
+      </RecipeSection>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#2D3436',
+    red: '#FAB1A0',
+    yellow: '#FFEAA7',
+    darkYellow: '#FDCB6E',
+    blue: '#74B9FF',
+    green: '#00B894',
+    darkGray: '#636E72',
+    lightGray: '#B2BEC3'
+  },
+
+  borderRadius: '2px',
+  easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
+
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    bold: 700,
+    black: 900
+  }
+};
+
+export default theme;
